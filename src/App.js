9977dc0697e2e005889017b5ff9ceb7285445540
Amitesh <amiteshjoshi231203@gmail.com>
2023-06-28 13:10:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+// import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Cart from './Components/Cart';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch('/api')
+    .then((res) => { return res.json() })
+    .then((data) => { console.log(data) })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Router>
+    <Header />
+    <div>
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/Cart' element={<Cart />}/>
+      </Routes>
     </div>
+    </Router>
+    </>
   );
 }
 
